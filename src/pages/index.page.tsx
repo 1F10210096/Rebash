e556ca72e651be4b362ee5f1ff15934b3259c08c
1F10210096 <s1F102100962@iniad.org>
2023-07-25 22:33:36 +0900

    css

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,4 +1,3 @@
-import type { TaskModel } from '$/commonTypesWithClient/models';
 import { useAtom } from 'jotai';
 import type { ChangeEvent, FormEvent } from 'react';
 import { useEffect, useState } from 'react';
@@ -10,10 +9,9 @@ import styles from './index.module.css';
 
 const Home = () => {
   const [user] = useAtom(userAtom);
-  const [tasks, setTasks] = useState<TaskModel[]>();
   const [roomId, setRoomId] = useState('');
   const [chat, setChat] = useState([]);
-  const [roomIdasse, setRooomIdasse] = useState(['a', 'b']);
+  // const [roomIdasse, setRooomIdasse] = useState(['a', 'b']);
   const inputRoomId = (e: ChangeEvent<HTMLInputElement>) => {
     setRoomId(e.target.value);
   };
@@ -22,54 +20,18 @@ const Home = () => {
     if (!user) return;
     const a = await apiClient.user.post({ body: { roomId } });
   };
-
-  const fetchTasks = async () => {
-    const tasks = await apiClient.tasks.$get().catch();
-
-    if (tasks !== null) setTasks(tasks);
-  };
-  const toggleDone = async (task: TaskModel) => {
-    await apiClient.tasks._taskId(task.id).patch({ body: { done: !task.done } });
-    await fetchTasks();
-  };
-  const deleteTask = async (task: TaskModel) => {
-    await apiClient.tasks._taskId(task.id).delete();
-    await fetchTasks();
-  };
-
   useEffect(() => {
     console.log('a');
   }, []);
 
-  if (!tasks || !user) return <Loading visible />;
+  if (!user) return <Loading visible />;
 
   return (
     <>
       <BasicHeader user={user} />
       <div className={styles.title} style={{ marginTop: '160px' }}>
-        Welcome to frourio!
+        Welcome to Rebash
       </div>
-
-      <form style={{ textAlign: 'center', marginTop: '80px' }} onSubmit={createTask}>
-        <input value={roomId} type="text" onChange={inputRoomId} />
-        <input type="submit" value="ADD" />
-      </form>
-      <ul className={styles.tasks}>
-        {tasks.map((task) => (
-          <li key={task.id}>
-            <label>
-              <input type="checkbox" checked={task.done} onChange={() => toggleDone(task)} />
-              <span>{task.label}</span>
-            </label>
-            <input
-              type="button"
-              value="DELETE"
-              className={styles.deleteBtn}
-              onClick={() => deleteTask(task)}
-            />
-          </li>
-        ))}
-      </ul>
     </>
   );
 };
