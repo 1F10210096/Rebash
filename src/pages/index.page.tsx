1507be66a754f56d9fedc0935bf7c4fa3b9cf955
1F10210096 <s1F102100962@iniad.org>
2023-08-14 16:24:02 +0900

    blueprint導入test

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,10 +1,25 @@
-import { Button } from '@blueprintjs/core';
+import React from 'react';
+import { Button, InputGroup, FocusStyleManager, Icon } from "@blueprintjs/core";
+import { IconNames } from "@blueprintjs/icons"; // 必要に応じて追加
 
 const HomePage = () => {
+  FocusStyleManager.onlyShowFocusOnTabs();
+
   const incrementCounter = () => {
-    console.log('a');
-  };
-  return <Button intent="success" text="button content" onClick={incrementCounter} />;
+    console.log("a")
+  }
+
+  return (
+    <div>
+      <Button icon="refresh" intent="danger" text="Reset" onClick={incrementCounter} />
+      <Button icon="user" rightIcon="caret-down" text="Profile settings" onClick={incrementCounter} />
+      <Button rightIcon="arrow-right" intent="success" text="Next step" onClick={incrementCounter} />
+      <Button>
+        <Icon icon="document" /> Upload... <Icon icon="small-cross" />
+      </Button>
+      <InputGroup leftIcon={IconNames.STAR} placeholder="Test me for focus" />
+    </div>
+  );
 };
 
 export default HomePage;
