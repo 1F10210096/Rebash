1c08ec94ccc0296fc0db9587bdf612729a99346e
1F10210096 <s1F102100962@iniad.org>
2023-08-14 17:17:18 +0900

    blueprint導入test

---
+++ src/pages/common/intentSelect.tsx
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2016 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as React from "react";
+
+import { Button, ControlGroup, HTMLSelect, Intent, Label } from "@blueprintjs/core";
+import { handleValueChange } from "@blueprintjs/docs-theme";
+
+const INTENTS = [
+  { label: "None", value: Intent.NONE },
+  { label: "Primary", value: Intent.PRIMARY },
+  { label: "Success", value: Intent.SUCCESS },
+  { label: "Warning", value: Intent.WARNING },
+  { label: "Danger", value: Intent.DANGER },
+];
+
+export interface IntentSelectProps {
+  intent: Intent;
+  label?: React.ReactNode;
+  onChange: (intent: Intent) => void;
+  /** @default false */
+  showClearButton?: boolean;
+}
+
+export const IntentSelect: React.FC<IntentSelectProps> = props => {
+  const handleChange = handleValueChange(props.onChange);
+  const handleClear = React.useCallback(() => props.onChange("none"), [props]);
+  return (
+    <Label>
+      {props.label}
+      <ControlGroup>
+        <HTMLSelect value={props.intent} onChange={handleChange} options={INTENTS} fill={true} />
+        {(props.showClearButton ?? false) && (
+          <Button aria-label="Clear" disabled={props.intent === "none"} icon="cross" onClick={handleClear} />
+        )}
+      </ControlGroup>
+    </Label>
+  );
+};
+IntentSelect.defaultProps = {
+  label: "Intent",
+};
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,24 +1,116 @@
-import React from 'react';
-import { Button, InputGroup, FocusStyleManager, Icon } from "@blueprintjs/core";
-import { IconNames } from "@blueprintjs/icons"; // 必要に応じて追加
+import React, { useState, useEffect } from 'react';
+import classNames from "classnames";
+import type { Alignment } from "@blueprintjs/core";
+import { Button, AnchorButton, H5, Code, Intent, Switch } from "@blueprintjs/core";
+import { Example, handleBooleanChange } from "@blueprintjs/docs-theme";
+import { Refresh, Duplicate } from "@blueprintjs/icons";
+import { AlignmentSelect } from "src/pages/common/alignmentSelect";
+import { IntentSelect } from "src/pages/common/intentSelect";
+import type { Size } from "src/pages/common/sizeSelect";
+import { SizeSelect } from "src/pages/common/sizeSelect";
 
 const HomePage = () => {
-  FocusStyleManager.onlyShowFocusOnTabs();
+  const [active, setActive] = useState(false);
+  const [alignText, setAlignText] = useState<Alignment | undefined>(undefined);
+  const [disabled, setDisabled] = useState(false);
+  const [fill, setFill] = useState(false);
+  const [iconOnly, setIconOnly] = useState(false);
+  const [intent, setIntent] = useState(Intent.NONE);
+  const [loading, setLoading] = useState(false);
+  const [minimal, setMinimal] = useState(false);
+  const [outlined, setOutlined] = useState(false);
+  const [size, setSize] = useState("regular");
+  const [wiggling, setWiggling] = useState(false);
+  const [wiggleTimeoutId, setWiggleTimeoutId] = useState(0);
 
-  const incrementCounter = () => {
-    console.log("a")
-  }
+  const handleActiveChange = handleBooleanChange(setActive);
+  const handleAlignTextChange = (alignText: Alignment) => setAlignText(alignText);
+  const handleDisabledChange = handleBooleanChange(setDisabled);
+  const handleFillChange = handleBooleanChange(setFill);
+  const handleIconOnlyChange = handleBooleanChange(setIconOnly);
+  const handleLoadingChange = handleBooleanChange(setLoading);
+  const handleMinimalChange = handleBooleanChange(setMinimal);
+  const handleOutlinedChange = handleBooleanChange(setOutlined);
+  const handleSizeChange = (size: Size) => setSize(size);
+  // const handleIntentChange = (intent: Intent) => setIntent(intent);
+
+  useEffect(() => {
+    return () => {
+      clearTimeout(wiggleTimeoutId);
+    };
+  }, [wiggleTimeoutId]);
+
+  const beginWiggling = () => {
+    clearTimeout(wiggleTimeoutId);
+    setWiggling(true);
+    const newTimeoutId = window.setTimeout(() => setWiggling(false), 300);
+    setWiggleTimeoutId(newTimeoutId);
+  };
+
+  const options = (
+    <>
+      <H5>Props</H5>
+      <Switch label="Active" checked={active} onChange={handleActiveChange} />
+      <Switch label="Disabled" checked={disabled} onChange={handleDisabledChange} />
+      <Switch label="Loading" checked={loading} onChange={handleLoadingChange} />
+      <Switch label="Minimal" checked={minimal} onChange={handleMinimalChange} />
+      <Switch label="Outlined" checked={outlined} onChange={handleOutlinedChange} />
+      <Switch label="Fill" checked={fill} onChange={handleFillChange} />
+      <AlignmentSelect align={alignText} onChange={handleAlignTextChange} />
+      {/* <SizeSelect size={size} onChange={handleSizeChange} /> */}
+      {/* <IntentSelect intent={intent} onChange={handleIntentChange} /> */}
+      <H5>Example</H5>
+      <Switch label="Icons only" checked={iconOnly} onChange={handleIconOnlyChange} />
+    </>
+  );
 
   return (
-    <div>
-      <Button icon="refresh" intent="danger" text="Reset" onClick={incrementCounter} />
-      <Button icon="user" rightIcon="caret-down" text="Profile settings" onClick={incrementCounter} />
-      <Button rightIcon="arrow-right" intent="success" text="Next step" onClick={incrementCounter} />
-      <Button>
-        <Icon icon="document" /> Upload... <Icon icon="small-cross" />
-      </Button>
-      <InputGroup leftIcon={IconNames.STAR} placeholder="Test me for focus" />
-    </div>
+    <Example options={options}>
+      <div className={classNames({ "docs-flex-column": fill })}>
+        <p>
+          <Code>Button</Code>
+        </p>
+        <Button
+          className={wiggling ? "docs-wiggle" : ""}
+          icon={<Refresh />}
+          onClick={beginWiggling}
+          small={size === "small"}
+          large={size === "large"}
+          active={active}
+          alignText={alignText}
+          disabled={disabled}
+          fill={fill}
+          intent={intent}
+          loading={loading}
+          minimal={minimal}
+          outlined={outlined}
+        >
+          {!iconOnly && "Click to wiggle"}
+        </Button>
+      </div>
+      <div className={classNames({ "docs-flex-column": fill })}>
+        <p>
+          <Code>AnchorButton</Code>
+        </p>
+        <AnchorButton
+          href="#core/components/buttons"
+          icon={<Duplicate />}
+          rightIcon="share"
+          target="_blank"
+          text={iconOnly ? undefined : "Duplicate this page"}
+          small={size === "small"}
+          large={size === "large"}
+          active={active}
+          alignText={alignText}
+          disabled={disabled}
+          fill={fill}
+          intent={intent}
+          loading={loading}
+          minimal={minimal}
+          outlined={outlined}
+        />
+      </div>
+    </Example>
   );
 };
 
