37b1bf0f40c63ca07ce6cbf71d39282428a53873
1F10210096 <s1F102100962@iniad.org>
2023-08-20 03:43:01 +0900

    friend一覧表示

--- src/pages/chat/index.page.tsx
+++ src/pages/chat/index.page.tsx
@@ -1,13 +1,19 @@
 import type { MessageModel } from '$/commonTypesWithClient/models';
 import {
   AppstoreOutlined,
+  BarChartOutlined,
   CheckOutlined,
+  CloudOutlined,
   MailOutlined,
   PlusOutlined,
   SearchOutlined,
   SendOutlined,
   SettingOutlined,
+  ShopOutlined,
+  TeamOutlined,
+  UploadOutlined,
   UserOutlined,
+  VideoCameraOutlined,
 } from '@ant-design/icons';
 import type { DatePickerProps, MenuProps } from 'antd';
 import {
@@ -142,6 +148,21 @@ const App: React.FC = () => {
       type,
     } as MenuItem;
   }
+
+  const friendMenu: MenuProps['items'] = [
+    UserOutlined,
+    VideoCameraOutlined,
+    UploadOutlined,
+    BarChartOutlined,
+    CloudOutlined,
+    AppstoreOutlined,
+    TeamOutlined,
+    ShopOutlined,
+  ].map((icon, index) => ({
+    key: String(index + 1),
+    icon: React.createElement(icon),
+    label: `nav ${index + 1}`,
+  }));
   const items: MenuProps['items'] = [
     getItem('Navigation One', 'sub1', <MailOutlined />, [
       getItem('Item 1', 'g1', null, [getItem('Option 1', '1'), getItem('Option 2', '2')], 'group'),
@@ -411,6 +432,37 @@ const App: React.FC = () => {
   }, [Roomlist, createUserdata]);
   return (
     <Layout hasSider>
+      <Sider
+        style={{
+          overflow: 'auto',
+          height: '100vh',
+          position: 'fixed',
+          left: 0,
+          top: 0,
+          bottom: 0,
+        }}
+      >
+        <div className="demo-logo-vertical" />
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={items} />
+      </Sider>
+      <Layout className="site-layout" style={{ marginLeft: 200 }}>
+        <Header style={{ padding: 0, background: colorBgContainer }} />
+        <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+          <div style={{ padding: 24, textAlign: 'center', background: colorBgContainer }}>
+            <p>long content</p>
+            {
+              // indicates very long content
+              Array.from({ length: 100 }, (_, index) => (
+                <React.Fragment key={index}>
+                  {index % 20 === 0 && index ? 'more' : '...'}
+                  <br />
+                </React.Fragment>
+              ))
+            }
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Ant Design ©2023 Created by Ant UED</Footer>
+      </Layout>
       {/* <>
         <Upload
           action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
