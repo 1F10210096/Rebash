c9ed5a02202d136589df81e9cddfd0e41606e66f
1F10210096 <s1F102100962@iniad.org>
2023-08-21 23:33:08 +0900

    scss

--- src/pages/title/index.page.tsx
+++ src/pages/title/index.page.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import './index.module.css';
+import styles from './index.module.scss'; // モジュール化されたスタイルをインポート
 
 const TitlePage = () => {
   return (
-    <div><div className="menu">
-      <div className="btn trigger">
-        <span className="line" />
+    <div className={styles.menu}>
+      <div className={`${styles.btn} ${styles.trigger}`}>
+        <span className={styles.line} />
       </div>
-      <div className="icons">
-        <div className="rotater">
-          <div className="btn btn-icon">
+      <div className={styles.icons}>
+        {/* 以下、他の rotater ブロックも同様に修正 */}
+        <div className={styles.rotater}>
+          <div className={`${styles.btn} ${styles.btnIcon}`}>
             <i className="fa fa-codepen" />
           </div>
         </div>
+        {/* 他の rotater ブロックも同様に修正 */}
       </div>
-    </div>
     </div>
   );
 };
