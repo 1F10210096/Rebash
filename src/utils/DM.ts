928a4803f422ec4bcc37d68a402fca71c5b04d0a
1F10210096 <s1F102100962@iniad.org>
2023-08-25 09:44:39 +0900

    DM

--- src/utils/DM.ts
+++ src/utils/DM.ts
@@ -2,6 +2,20 @@ import { useAtom } from 'jotai';
 import { userAtom } from 'src/atoms/user';
 import { apiClient } from './apiClient';
 
+export function useCreateDM() {
+  const [user] = useAtom(userAtom);
+
+  async function createDM(partnerId: string) {
+    if (!user) return;
+
+    const userId = user.id;
+
+    const createDM = await apiClient.dm.post({ body: { partnerId, userId } });
+    return createDM;
+  }
+
+  return createDM;
+}
 export function useSearchDM() {
   const [user] = useAtom(userAtom);
 
@@ -10,9 +24,8 @@ export function useSearchDM() {
 
     const userId = user.id;
 
-    const serchDM = await apiClient.dm.post({ body: { partnerId, userId } });
-    await apiClient.dmuser.post({ body: { partnerId, userId } });
-    return serchDM;
+    const searchDM = await apiClient.dmuser.post({ body: { partnerId, userId } });
+    return searchDM;
   }
 
   return searchDM;
