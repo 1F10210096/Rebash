3ac9917989bf94f4b1a8fc2279620b6c905967a2
1F10210096 <s1F102100962@iniad.org>
2023-09-03 19:00:39 +0900

    ファイル管理

--- src/pages/upload/index.page.tsx
+++ src/pages/upload/index.page.tsx
@@ -1,1 +1,68 @@
-// 
+import React, { useState } from 'react';
+import { PlusOutlined } from '@ant-design/icons';
+import { Modal, Upload } from 'antd';
+import type { RcFile, UploadProps } from 'antd/es/upload';
+import type { UploadFile } from 'antd/es/upload/interface';
+
+const getBase64 = (file: RcFile): Promise<string> =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result as string);
+    reader.onerror = (error) => reject(error);
+  });
+
+const App: React.FC = () => {
+  const [previewOpen, setPreviewOpen] = useState(false);
+  const [previewImage, setPreviewImage] = useState('');
+  const [previewTitle, setPreviewTitle] = useState('');
+  const [fileList, setFileList] = useState<UploadFile[]>([
+
+  ]);
+
+  const handleCancel = () => setPreviewOpen(false);
+
+  // eslint-disable-next-line complexity
+  const handlePreview = async (file: UploadFile) => {
+    if ((file.url === null) && (file.preview === null)) {
+      file.preview = await getBase64(file.originFileObj as RcFile);
+    }
+
+    setPreviewImage(file.url ?? (file.preview as string) ?? '');
+    setPreviewOpen(true);
+    if (file.url !== null && file.url !== undefined) {
+      setPreviewTitle(file.name || file.url.substring(file.url.lastIndexOf('/') + 1));
+    } else {
+      // Handle the case when file.url is null or undefined
+      setPreviewTitle(file.name || '');
+    }
+  };
+
+  const handleChange: UploadProps['onChange'] = ({ fileList: newFileList }) =>
+    setFileList(newFileList);
+
+  const uploadButton = (
+    <div>
+      <PlusOutlined />
+      <div style={{ marginTop: 8 }}>Upload</div>
+    </div>
+  );
+  return (
+    <>
+      <Upload
+        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+        listType="picture-circle"
+        fileList={fileList}
+        onPreview={handlePreview}
+        onChange={handleChange}
+      >
+        {fileList.length >= 8 ? null : uploadButton}
+      </Upload>
+      <Modal open={previewOpen} title={previewTitle} footer={null} onCancel={handleCancel}>
+        <img alt="example" style={{ width: '100%' }} src={previewImage} />
+      </Modal>
+    </>
+  );
+};
+
+export default App;
